25b1bac7f6e170370fdf0e4dd975c11e1ed8985a
deborahmanik <debimanik06@gmail.com>
2024-05-04 08:23:35 +0700

    first

---
+++ modul10_1302220129/Mahasiswa.cs
@@ -0,0 +1,37 @@
+﻿namespace modul10_1302220129
+{
+    public class Mahasiswa
+    {
+        public String nama ( get; set; )
+        public String nim (get; set; )
+        public List<string> course;
+        public int year;
+
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nim = nim;
+            this.nama = nama;
+        }
+
+        public void setNama (String nama)
+        {
+            this.nama = nama;
+        }
+
+        public String getNama()
+        {
+            return nama();
+        }
+
+        public String getnim()
+        {
+            return nim();
+        }
+
+        public void setNim(String nim)
+        {
+            this.nim = nim;
+        }
+
+    }
+}
